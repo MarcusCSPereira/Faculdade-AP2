e7a64edccfb47a3b4d4d2b8657b533a1e41a457b
Marcus César Santos Pereira <marcus.cesar.sp@gmail.com>
2023-10-27 19:02:41 -0300

    commit

--- Lista_08/Ex001/App.java
+++ Lista_08/Ex001/App.java
@@ -1,3 +1,29 @@
 public class App {
-    
+    public static void main(String[] args) {
+        InteiroSet conjunto1 = new InteiroSet();
+        InteiroSet conjunto2 = new InteiroSet();
+
+        conjunto1.insereElemento(10);
+        conjunto1.insereElemento(20);
+        conjunto1.insereElemento(30);
+
+        conjunto2.insereElemento(20);
+        conjunto2.insereElemento(30);
+        conjunto2.insereElemento(40);
+
+        System.out.println("Conjunto 1: " + conjunto1.toSetString());
+        System.out.println("Conjunto 2: " + conjunto2.toSetString());
+
+        InteiroSet uniao = conjunto1.union(conjunto2);
+        System.out.println("União: " + uniao.toSetString());
+
+        InteiroSet interseccao = conjunto1.interseccao(conjunto2);
+        System.out.println("Intersecção: " + interseccao.toSetString());
+
+        conjunto1.deleteElemento(20);
+        System.out.println("Conjunto 1 após exclusão do elemento 20: " + conjunto1.toSetString());
+
+        boolean iguais = conjunto1.ehIgualTo(conjunto2);
+        System.out.println("Conjunto 1 e Conjunto 2 são iguais? " + iguais);
+    }
 }
--- Lista_08/Ex001/InteiroSet.java
+++ Lista_08/Ex001/InteiroSet.java
@@ -1,12 +1,69 @@
 import java.util.ArrayList;
 
 public class InteiroSet {
+    
     Boolean conjunto[];
-    public InteiroSet(){    
-        
 
+    public InteiroSet() {
+        conjunto = new Boolean[101];
+        for (int i = 0; i < 101; i++) {
+            conjunto[i] = false;
+        }
     }
-    
 
-    
+    public InteiroSet union(InteiroSet outroConjunto) {
+        InteiroSet uniao = new InteiroSet();
+        for (int i = 0; i < 101; i++) {
+            uniao.conjunto[i] = this.conjunto[i] || outroConjunto.conjunto[i];
+        }
+        return uniao;
+    }
+
+    public InteiroSet interseccao(InteiroSet outroConjunto) {
+        InteiroSet interseccao = new InteiroSet();
+        for (int i = 0; i < 101; i++) {
+            interseccao.conjunto[i] = this.conjunto[i] && outroConjunto.conjunto[i];
+        }
+        return interseccao;
+    }
+
+    public void insereElemento(int k) {
+        if (k >= 0 && k <= 100) {
+            conjunto[k] = true;
+        }
+    }
+
+    public void deleteElemento(int m) {
+        if (m >= 0 && m <= 100) {
+            conjunto[m] = false;
+        }
+    }
+
+    public String toSetString() {
+        ArrayList<Integer> elementos = new ArrayList<Integer>();
+        for (int i = 0; i < 101; i++) {
+            if (conjunto[i]) {
+                elementos.add(i);
+            }
+        }
+        if (elementos.size() == 0) {
+            return "-";
+        } else {
+            String setString = "";
+            for (int i = 0; i < elementos.size() - 1; i++) {
+                setString += elementos.get(i) + " ";
+            }
+            setString += elementos.get(elementos.size() - 1);
+            return setString;
+        }
+    }
+
+    public boolean ehIgualTo(InteiroSet outroConjunto) {
+        for (int i = 0; i < 101; i++) {
+            if (this.conjunto[i] != outroConjunto.conjunto[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
